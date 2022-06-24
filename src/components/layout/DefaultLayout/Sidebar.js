8f87d3de815ea56ddf8f7926a53bef884f229c2b
Trần Hữu Toàn <jungleoffice02@gmail.com>
2022-06-24 23:21:12 +0700

    Navbar, sidebar

--- src/components/layout/DefaultLayout/Sidebar.js
+++ src/components/layout/DefaultLayout/Sidebar.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { FaHome } from "react-icons/fa";
+import { BsPeople } from "react-icons/bs";
+import { BsCameraVideo } from "react-icons/bs";
 
 function Sidebar() {
-  return <div class="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__tabs">
+        <div className="sidebar__tab">
+          <FaHome className="sidebar__icon sidebar__active" />
+          <h2 className="sidebar__title sidebar__active">Dành cho bạn</h2>
+        </div>
+        <div className="sidebar__tab">
+          <BsPeople className="sidebar__icon" />
+          <h2 className="sidebar__title">Đang Follow</h2>
+        </div>
+        <div className="sidebar__tab">
+          <BsCameraVideo className="sidebar__icon" />
+          <h2 className="sidebar__title">LIVE</h2>
+        </div>
+      </div>
+      <hr className="hr" />
+      <div className="sidebar__suggest">
+        <p className="sidebar__suggest__title">Tài khoản được đề xuất</p>
+        <div className="sidebar__suggest__account">Trần Hữu Toàn</div>
+        <div className="sidebar__suggest__account">Trần Hữu Toàn</div>
+        <div className="sidebar__suggest__account">Trần Hữu Toàn</div>
+        <div className="sidebar__suggest__account">Trần Hữu Toàn</div>
+        <div className="sidebar__suggest__account">Trần Hữu Toàn</div>
+        <div>
+          <p className="sidebar__suggest__seeAll">Xem tất cả</p>
+        </div>
+      </div>
+      <hr className="hr" />
+    </div>
+  );
 }
 
 export default Sidebar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      <div>
+        <video
+          src="https://v16-webapp.tiktok.com/25f6127594c751c5b0a012999679a3cb/62b63237/video/tos/useast2a/tos-useast2a-pve-0037-aiso/f9855bb0ddee4f2baff6cacd5661b994/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=3300&bt=1650&btag=80000&cs=0&ds=3&ft=eXd.6Hk_Myq8Zb-ZUwe2Nskhml7Gb&mime_type=video_mp4&qs=0&rc=Nzg6Mzg4Z2U7OWk3ZmY6ZkBpMzg1O2k6ZnlwZDMzZjgzM0AwMTAuNV4wNTExLl8vYTAxYSMwZDFfcjRnc2ZgLS1kL2Nzcw%3D%3D&l=2022062415522601024402618916295097"
+          loop
+          autoPlay
+          muted
+        ></video>
+      </div>
+      <div>
+        <video
+          src="https://v16-webapp.tiktok.com/25f6127594c751c5b0a012999679a3cb/62b63237/video/tos/useast2a/tos-useast2a-pve-0037-aiso/f9855bb0ddee4f2baff6cacd5661b994/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=3300&bt=1650&btag=80000&cs=0&ds=3&ft=eXd.6Hk_Myq8Zb-ZUwe2Nskhml7Gb&mime_type=video_mp4&qs=0&rc=Nzg6Mzg4Z2U7OWk3ZmY6ZkBpMzg1O2k6ZnlwZDMzZjgzM0AwMTAuNV4wNTExLl8vYTAxYSMwZDFfcjRnc2ZgLS1kL2Nzcw%3D%3D&l=2022062415522601024402618916295097"
+          loop
+          autoPlay
+          muted
+        ></video>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
