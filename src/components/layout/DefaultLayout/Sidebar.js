2ab8aa640a73e1fb01def45d5fd11991a0a1e6af
Tran Toan <huutrantoan@gmail.com>
2022-07-13 21:52:17 +0700

    CSS sidebar, navbar, home

--- src/components/layout/DefaultLayout/Sidebar.js
+++ src/components/layout/DefaultLayout/Sidebar.js
@@ -4,6 +4,7 @@ import { BsPeople } from "react-icons/bs";
 import { BsCameraVideo } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import Avatar from "~/assets/images/Avatar.jpeg";
+import Verify from "~/assets/images/verify.svg";
 
 function Sidebar() {
   const [isActive, setIsActive] = useState(1);
@@ -14,115 +15,132 @@ function Sidebar() {
 
   return (
     <div className="sidebar">
-      <div className="sidebar__tabs">
-        <Link
-          to="/"
-          className={
-            isActive === 1 ? "sidebar__tab sidebar__active" : "sidebar__tab"
-          }
-          onClick={() => handleClick(1)}
-        >
-          <FaHome className="sidebar__icon" />
-          <h2 className="sidebar__title">Dành cho bạn</h2>
-        </Link>
-        <Link
-          to="/following"
-          className={
-            isActive === 2 ? "sidebar__tab sidebar__active" : "sidebar__tab"
-          }
-          onClick={() => handleClick(2)}
-        >
-          <BsPeople className="sidebar__icon" />
-          <h2 className="sidebar__title">Đang Follow</h2>
-        </Link>
-        <Link
-          to="/live"
-          className={
-            isActive === 3 ? "sidebar__tab sidebar__active" : "sidebar__tab"
-          }
-          onClick={() => handleClick(3)}
-        >
-          <BsCameraVideo className="sidebar__icon" />
-          <h2 className="sidebar__title">LIVE</h2>
-        </Link>
-      </div>
-      <hr className="hr" />
-      <div className="sidebar__list">
-        <p className="sidebar__title">Tài khoản được đề xuất</p>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
+      <div className="sidebar__scrollbar">
+        <div className="sidebar__tabs">
+          <Link
+            to="/"
+            className={
+              isActive === 1 ? "sidebar__tab sidebar__active" : "sidebar__tab"
+            }
+            onClick={() => handleClick(1)}
+          >
+            <FaHome className="sidebar__icon" />
+            <h2 className="sidebar__title">Dành cho bạn</h2>
+          </Link>
+          <Link
+            to="/following"
+            className={
+              isActive === 2 ? "sidebar__tab sidebar__active" : "sidebar__tab"
+            }
+            onClick={() => handleClick(2)}
+          >
+            <BsPeople className="sidebar__icon" />
+            <h2 className="sidebar__title">Đang Follow</h2>
+          </Link>
+          <Link
+            to="/live"
+            className={
+              isActive === 3 ? "sidebar__tab sidebar__active" : "sidebar__tab"
+            }
+            onClick={() => handleClick(3)}
+          >
+            <BsCameraVideo className="sidebar__icon" />
+            <h2 className="sidebar__title">LIVE</h2>
+          </Link>
         </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
+        <hr className="hr" />
+        <div className="sidebar__list">
+          <p className="sidebar__title">Tài khoản được đề xuất</p>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div>
+            <p className="sidebar__seeAll">Xem tất cả</p>
+          </div>
         </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
+        <hr className="hr" />
+        <div className="sidebar__list">
+          <p className="sidebar__title">Các tài khoản đang follow</p>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div className="sidebar__item">
+            <img className="sidebar__item__avatar" src={Avatar} alt="" />
+            <div className="sidebar__item__name">cristiano_ronaldo</div>
+            <img className='verify__author' src={Verify} alt='' />
+          </div>
+          <div>
+            <p className="sidebar__seeAll">Xem tất cả</p>
+          </div>
         </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
+        <hr className="hr" />
+        <div className="sidebar__list">
+          <p className="sidebar__title">Khám phá</p>
         </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div>
-          <p className="sidebar__seeAll">Xem tất cả</p>
-        </div>
-      </div>
-      <hr className="hr" />
-      <div className="sidebar__list">
-        <p className="sidebar__title">Các tài khoản đang follow</p>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div className="sidebar__item">
-          <img className="sidebar__item__avatar" src={Avatar} alt="" />
-          <div className="sidebar__item__name">cristiano_ronaldo</div>
-        </div>
-        <div>
-          <p className="sidebar__seeAll">Xem tất cả</p>
-        </div>
-      </div>
-      <hr className="hr" />
-      <div className="sidebar__list">
-        <p className="sidebar__title">Khám phá</p>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,32 +6,63 @@ import Verify from "~/assets/images/verify.svg";
 function Home() {
   return (
     <div className="home">
-      <div className="recommend__list__item__container">
-        <img className="avatar-author" src={Avatar} alt="" />
-        <div className="content__container">
-          <div className="text__info__container">
-            <div className="text__info">
-              <div className="author__container">
-                <h3 className="username__author">cristiano_ronaldo</h3>
-                <img src={Verify} alt="" />
-                <h3 className="name__author">Cristiano Ronaldo</h3>
+      <div className="main__container">
+        <div className="recommend__list__item__container">
+          <img className="avatar-author" src={Avatar} alt="" />
+          <div className="content__container">
+            <div className="text__info__container">
+              <div className="text__info">
+                <div className="author__container">
+                  <h3 className="username__author">cristiano_ronaldo</h3>
+                  <img src={Verify} alt="" />
+                  <h3 className="name__author">Cristiano Ronaldo</h3>
+                </div>
+
+                <span className="video__desc__text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                  sequi ea assumenda culpa eos totam placeat recusandae nam
+                  reprehenderit distinctio itaque animi, delectus facere harum a
+                  unde exercitationem nulla velit?
+                </span>
               </div>
 
-              <span className="video__desc__text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                sequi ea assumenda culpa eos totam placeat recusandae nam
-                reprehenderit distinctio itaque animi, delectus facere harum a
-                unde exercitationem nulla velit?
-              </span>
+              <button className="button__follow__author">Follow</button>
             </div>
-
-            <button className="button__follow__author">Follow</button>
+            <div className="video__wrapper">
+              <div className="video__card">
+                <video controls src={Siu} loop autoPlay muted></video>
+              </div>
+              <div className="action__item">Like</div>
+            </div>
           </div>
-          <div className="video__wrapper">
-            <div className="video__card">
-              <video controls src={Siu} loop autoPlay muted></video>
+        </div>
+        <div className="recommend__list__item__container">
+          <img className="avatar-author" src={Avatar} alt="" />
+          <div className="content__container">
+            <div className="text__info__container">
+              <div className="text__info">
+                <div className="author__container">
+                  <h3 className="username__author">cristiano_ronaldo</h3>
+                  <img src={Verify} alt="" />
+                  <h3 className="name__author">Cristiano Ronaldo</h3>
+                </div>
+
+                <span className="video__desc__text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                  sequi ea assumenda culpa eos totam placeat recusandae nam
+                  reprehenderit distinctio itaque animi, delectus facere harum a
+                  unde exercitationem nulla velit?
+                </span>
+              </div>
+
+              <button className="button__follow__author">Follow</button>
             </div>
-            <div className="action__item">Like</div>
+            <div className="video__wrapper">
+              <div className="video__card">
+                <video controls src={Siu} loop autoPlay muted></video>
+              </div>
+              <div className="action__item">Like</div>
+            </div>
           </div>
         </div>
       </div>
