28d18747f8e65f8be0ec8c79e9bb55e0d56c801c
Tran Toan <huutrantoan@gmail.com>
2022-07-15 13:10:51 +0700

    Add action items

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Siu from "~/assets/videos/Siu.mp4";
 import Avatar from "~/assets/images/Avatar.jpeg";
 import Verify from "~/assets/images/verify.svg";
+import { FaHeart, FaRegCommentDots, FaShare } from "react-icons/fa";
 
 function Home() {
   return (
@@ -19,10 +20,10 @@ function Home() {
                 </div>
 
                 <span className="video__desc__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                  sequi ea assumenda culpa eos totam placeat recusandae nam
-                  reprehenderit distinctio itaque animi, delectus facere harum a
-                  unde exercitationem nulla velit?
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ratione, incidunt non distinctio nemo saepe nulla architecto
+                  quam id nihil corporis et amet? Corporis dolorum sed alias
+                  expedita iste
                 </span>
               </div>
 
@@ -32,36 +33,20 @@ function Home() {
               <div className="video__card">
                 <video controls src={Siu} loop autoPlay muted></video>
               </div>
-              <div className="action__item">Like</div>
-            </div>
-          </div>
-        </div>
-        <div className="recommend__list__item__container">
-          <img className="avatar-author" src={Avatar} alt="" />
-          <div className="content__container">
-            <div className="text__info__container">
-              <div className="text__info">
-                <div className="author__container">
-                  <h3 className="username__author">cristiano_ronaldo</h3>
-                  <img src={Verify} alt="" />
-                  <h3 className="name__author">Cristiano Ronaldo</h3>
+              <div className="action__items">
+                <div className="like__button">
+                  <FaHeart className="icon" />
+                  <strong className="count">484.3K</strong>
                 </div>
-
-                <span className="video__desc__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                  sequi ea assumenda culpa eos totam placeat recusandae nam
-                  reprehenderit distinctio itaque animi, delectus facere harum a
-                  unde exercitationem nulla velit?
-                </span>
+                <div className="comment__button">
+                  <FaRegCommentDots className="icon" />
+                  <strong className="count">502</strong>
+                </div>
+                <div className="share__button">
+                  <FaShare className="icon" />
+                  <strong className="count">7482</strong>
+                </div>
               </div>
-
-              <button className="button__follow__author">Follow</button>
-            </div>
-            <div className="video__wrapper">
-              <div className="video__card">
-                <video controls src={Siu} loop autoPlay muted></video>
-              </div>
-              <div className="action__item">Like</div>
             </div>
           </div>
         </div>
