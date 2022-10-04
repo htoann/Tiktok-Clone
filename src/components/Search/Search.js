d0488f0b8601ef169ffcc46ddc74a3d707618ee7
htoan <huutrantoan@gmail.com>
2022-10-04 10:35:08 +0700

    Search Component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,28 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import PopperWrapper from "~/components/Popper/Wrapper";
 import AccountItem from "~/components/AccountItem/AccountItem";
 import Tippy from "@tippyjs/react/headless";
 import "tippy.js/dist/tippy.css";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaSpinner, FaTimesCircle } from "react-icons/fa";
 import styles from "./Search.module.scss";
 
 function Search() {
   const [searchValue, setSearchValue] = useState("");
   const [searchResult, setSearchResult] = useState([]);
+  const [showResult, setShowResult] = useState(true);
+
+  const searchInput = useRef(null);
 
   setTimeout(() => {
     setSearchResult([1, 2]);
   }, 1000);
 
-  const handleChange = (e) => {
-    setSearchValue(e.target.value);
+  const handleClear = () => {
+    setSearchResult([]);
+    setSearchValue("");
+    searchInput.current.focus();
+  };
+
+  const handleHideResult = () => {
+    setShowResult(false);
+  };
+
+  const handleShowResult = () => {
+    setShowResult(true);
   };
 
   return (
     <div>
       <Tippy
-        visible={searchResult.length > 0}
+        visible={showResult && searchResult.length > 0}
         interactive
+        onClickOutside={handleHideResult}
         render={(attrs) => (
           <div className={styles.search_results} tabIndex="-1" {...attrs}>
             <PopperWrapper>
@@ -35,12 +49,18 @@ function Search() {
       >
         <form className={styles.navbar_search}>
           <input
+            value={searchValue}
             className={styles.navbar_search_input}
             type="text"
             placeholder="Search accounts and videos"
-            onChange={handleChange}
+            onChange={(e) => setSearchValue(e.target.value)}
+            ref={searchInput}
+            onFocus={handleShowResult}
           />
-          {searchValue && <p>Hello</p>}
+          {searchValue && (
+            <FaTimesCircle className={styles.clear} onClick={handleClear} />
+          )}
+          {/* <FaSpinner className={styles.loading} /> */}
           <span className={styles.navbar_search_line}></span>
           <button className={styles.navbar_search_icon}>
             <FaSearch />
