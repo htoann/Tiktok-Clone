2c1de4f22ccbc0af68032e8cafba1ca69ecffde0
Tran Toan <huutrantoan@gmail.com>
2022-10-13 18:34:19 +0700

    fix: vercel react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-// import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 import { publicRoutes } from "./routes/index";
 import "./static/sass/styles.scss";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="App">
         <Routes>{publicRoutes}</Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
-  s;
 }
 
 export default App;
