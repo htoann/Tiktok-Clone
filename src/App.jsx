0b99b9e32181ed6461f931ca1be96a9cd7844b5d
Tran Toan <huutrantoan@gmail.com>
2022-10-19 12:08:56 +0700

    refactor: router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { BrowserRouter as Router, Routes } from "react-router-dom";
-import { publicRoutes } from "./routes/index";
+import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { publicRoutes } from "./routes";
+import { DefaultLayout } from "~/layouts";
 import "./static/sass/styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>{publicRoutes}</Routes>
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            let Layout = DefaultLayout;
+
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
       </div>
     </Router>
   );
