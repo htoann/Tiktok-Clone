95e034a88bbe17298981a66fa0c69585d4c2c762
Tran Toan <huutrantoan@gmail.com>
2022-10-19 21:14:45 +0700

    refactor: language file data

--- src/data/dataMenu.js
+++ src/data/dataMenu.js
@@ -6,7 +6,6 @@ import {
   FaBitcoin,
   FaRegSun,
   FaSignOutAlt,
-  FaAffiliatetheme,
   FaCode,
   FaRegPaperPlane,
   FaFacebook,
@@ -14,6 +13,7 @@ import {
   FaLink,
 } from "react-icons/fa";
 import React from "react";
+import languages from "./languages";
 
 export const MENU_ITEMS_1 = [
   {
@@ -21,39 +21,7 @@ export const MENU_ITEMS_1 = [
     icon: <FaAutoprefixer />,
     children: {
       title: "Language",
-      data: [
-        { type: "language", code: "en", title: "English" },
-        { type: "language", code: "vi", title: "Tiếng Việt" },
-        { type: "languea", title: "বাঙ্গালি (ভারত)" },
-        { type: "languea", title: "Cebuano (Pilipinas)" },
-        { type: "languea", title: "Čeština (Česká republika)" },
-        { type: "languea", title: "Deutsch" },
-        { type: "languea", title: "Ελληνικά (Ελλάδα)" },
-        { type: "languea", title: "Español" },
-        { type: "languea", title: "Suomi (Suomi)" },
-        { type: "languea", title: "Filipino (Pilipinas)" },
-        { type: "languea", title: "Français" },
-        { type: "languea", title: "हिंदी}" },
-        { type: "languea", title: "Magyar (Magyarország)" },
-        { type: "languea", title: "Bahasa Indonesia" },
-        { type: "languea", title: "Italiano (Italia)" },
-        { type: "languea", title: "日本語（日本）" },
-        { type: "languea", title: "Basa Jawa (Indonesia)" },
-        { type: "languea", title: "ខ្មែរ (កម្ពុជា)" },
-        { type: "languea", title: "한국어 (대한민국)" },
-        { type: "languea", title: "Bahasa Melayu (Malaysia)" },
-        { type: "languea", title: "မြန်မာ (မြန်မာ)" },
-        { type: "languea", title: "Nederlands (Nederland)" },
-        { type: "languea", title: "Polski (Polska)" },
-        { type: "languea", title: "Português (Brasil)" },
-        { type: "languea", title: "Română (Romania)" },
-        { type: "languea", title: "Русский (Россия)" },
-        { type: "languea", title: "Svenska (Sverige)" },
-        { type: "languea", title: "ไทย (ไทย)" },
-        { type: "languea", title: "Türkçe (Türkiye)" },
-        { type: "languea", title: "Українська (Україна)" },
-        { type: "languea", title: "简体中文" },
-      ],
+      data: languages,
     },
   },
   { title: "Feedback and help", icon: <FaQuestion /> },
@@ -61,7 +29,7 @@ export const MENU_ITEMS_1 = [
 ];
 
 export const MENU_ITEMS_2 = [
-  { title: "View profile", icon: <FaRegUser /> },
+  { title: "View profile", icon: <FaRegUser />, type: "toProfile" },
   { title: "Get coins", icon: <FaBitcoin /> },
   { title: "Settings", icon: <FaRegSun /> },
   ...MENU_ITEMS_1,
