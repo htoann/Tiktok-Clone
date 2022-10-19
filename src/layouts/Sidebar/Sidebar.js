8d645d0042998021f551c4fe5e2ee6008cb50648
Tran Toan <huutrantoan@gmail.com>
2022-10-19 21:13:43 +0700

    fix: css

--- src/layouts/Sidebar/Sidebar.js
+++ src/layouts/Sidebar/Sidebar.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { FaHome } from "react-icons/fa";
-import { BsPeople } from "react-icons/bs";
-import { BsCameraVideo } from "react-icons/bs";
+import { BsPeople, BsCameraVideo } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import ListAccount from "~/components/ListAccount/ListAccounts.js";
-import Button from "~/components/Button/Button";
+import ListAccount from "~/components/ListAccount";
+import Button from "~/components/Button";
 import styles from "./Sidebar.module.scss";
 import { getUsersService } from "~/services/getUsersService";
 import { config } from "~/config";
+import { useSelector } from "react-redux";
 
 function Sidebar() {
   const [isActive, setIsActive] = useState(1);
-  const [user, setUser] = useState([]);
+  const { user } = useSelector((state) => state.user);
   const [suggestedList, setSuggestedList] = useState([]);
   const [followingList, setFollowingList] = useState([]);
   const [perpage, setPerpage] = useState(5);
@@ -86,9 +86,11 @@ function Sidebar() {
               <p className={styles.login_tip}>
                 Log in to follow creators, like videos, and view comments.
               </p>
-              <Button outline large>
-                Log in
-              </Button>
+              <Link to={config.routes.login}>
+                <Button outline large className={styles.button_login}>
+                  Log in
+                </Button>
+              </Link>
             </div>
             <hr className={styles.hr} />
           </>
@@ -101,10 +103,15 @@ function Sidebar() {
           perpage={perpage}
         />
         <ListAccount title="Following accounts" list={followingList} />
-        <p>Contact me</p>
-        <a className={styles.facebook} href="https://www.facebook.com/htoann/">
-          Trần Hữu Toàn
-        </a>
+        <div className={styles.contact}>
+          <p>Contact me</p>
+          <a
+            className={styles.facebook}
+            href="https://www.facebook.com/htoann/"
+          >
+            Trần Hữu Toàn
+          </a>
+        </div>
       </div>
     </div>
   );
