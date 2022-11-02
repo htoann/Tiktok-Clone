e40f46689ccbeb81ebeaeeb848ace129cb6b1102
htoann <huutrantoan@gmail.com>
2022-11-02 13:01:38 +0700

    fix: get full name

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,8 @@
 export const getFullName = (user) => {
-  return user && (user.fullname || user.first_name + " " + user.last_name);
+  return (
+    user &&
+    (user.fullname ||
+      (user.first_name !== "" && user.first_name + " " + user.last_name) ||
+      user.nickname)
+  );
 };
