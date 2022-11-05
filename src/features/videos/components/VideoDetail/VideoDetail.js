8e48c68bf662a71de1af4e4a98426a38ad3563f6
Tran Toan <huutrantoan@gmail.com>
2022-11-05 16:15:10 +0700

    fix: unmute video, modal video detail

--- src/features/videos/components/VideoDetail/VideoDetail.js
+++ src/features/videos/components/VideoDetail/VideoDetail.js
@@ -5,20 +5,17 @@ import ListComment from "~/features/comments/components/ListComment";
 
 function VideoDetail({ video }) {
   return (
-    <>
-      <div className={styles.wrapper}>
-        <div className={styles.video_container}>
-          <img className={styles.blur} src={video.thumb_url} alt="" />
-          <div className={styles.video_wrapper}>
-            <div className={styles.video}>
-              <Video data={video} />
-            </div>
+    <div className={styles.wrapper}>
+      <div className={styles.video_container}>
+        <img className={styles.blur} src={video.thumb_url} alt="" />
+        <div className={styles.video_wrapper}>
+          <div className={styles.video}>
+            <Video data={video} />
           </div>
         </div>
-        <ListComment video={video} />
       </div>
-      <Video data={video} />
-    </>
+      <ListComment video={video} />
+    </div>
   );
 }
 
