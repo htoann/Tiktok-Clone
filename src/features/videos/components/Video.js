099c8ffeca40c13f41ef32cb5f2bb9f050ccb187
Tran Toan <huutrantoan@gmail.com>
2022-11-07 20:15:38 +0700

    feat: null data for video conponent

--- src/features/videos/components/Video.js
+++ src/features/videos/components/Video.js
@@ -5,8 +5,7 @@ function Video({ data }) {
   // Automatically Play and Pause video as it enters and leaves the viewport/screen
   const videoRef = useRef(null);
   const [showControls, setShowControls] = useState(false);
-  const videoTime = data.meta?.playtime_seconds;
-
+  const videoTime = data.meta?.playtime_seconds || 0;
   const handleShowControls = () => {
     setShowControls(true);
   };
@@ -57,7 +56,7 @@ function Video({ data }) {
       disablePictureInPicture
       controlsList="nofullscreen nodownload noremoteplayback noplaybackrate"
       onMouseEnter={handleShowControls}
-      poster={data.thumb_url}
+      poster={data.thumb_url || ""}
       onClick={handlePauseVideo}
       loop
       autoPlay
