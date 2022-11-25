a1f4d3312105c7350a8cb0d3fd889de6f8aa9a6a
Tran Toan <huutrantoan@gmail.com>
2022-11-25 17:09:45 +0700

    feat: navlink

--- src/layouts/Sidebar/Menu/Menu.js
+++ src/layouts/Sidebar/Menu/Menu.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import React from "react";
 import styles from "./Menu.module.scss";
 import { BsCameraVideo, BsPeople } from "react-icons/bs";
 import { FaHome } from "react-icons/fa";
@@ -8,54 +7,20 @@ import WrapperAuth from "~/components/WrapperAuth";
 import Tab from "./Tab";
 
 function Menu() {
-  const [isActive, setIsActive] = useState("/");
-  const location = useLocation();
-
-  useEffect(() => {
-    setIsActive(location.pathname);
-  }, [location.pathname]);
-
   return (
-    <>
-      <div className={styles.sidebar_tabs}>
-        <Tab
-          to={config.routes.home}
-          Icon={FaHome}
-          className={
-            isActive === "/"
-              ? `${styles.sidebar_tab} ${styles.sidebar_active}`
-              : styles.sidebar_tab
-          }
-        >
-          For you
+    <nav className={styles.sidebar_tabs}>
+      <Tab to={config.routes.home} Icon={FaHome}>
+        For you
+      </Tab>
+      <WrapperAuth>
+        <Tab to={config.routes.following} Icon={BsPeople}>
+          Following
         </Tab>
-        <WrapperAuth>
-          <Tab
-            to={config.routes.following}
-            Icon={BsPeople}
-            className={
-              isActive === "/following"
-                ? `${styles.sidebar_tab} ${styles.sidebar_active}`
-                : styles.sidebar_tab
-            }
-          >
-            Following
-          </Tab>
-        </WrapperAuth>
-        <Tab
-          to={config.routes.live}
-          Icon={BsCameraVideo}
-          className={
-            isActive === "/live"
-              ? `${styles.sidebar_tab} ${styles.sidebar_active}`
-              : styles.sidebar_tab
-          }
-        >
-          Live
-        </Tab>
-      </div>
-      <hr className={styles.hr} />
-    </>
+      </WrapperAuth>
+      <Tab to={config.routes.live} Icon={BsCameraVideo}>
+        Live
+      </Tab>
+    </nav>
   );
 }
 
